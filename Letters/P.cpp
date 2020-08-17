85e69dae4d76c7ba07715e581aa7fdaa9ba46d9d
anilberg <anilberg@users.noreply.github.com>
2020-08-17 22:26:07 +0300

    New Letters by Anıl

---
+++ Letters/P.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    for(int i = 1; i < 6; i++){
+        for(int j = 1; j < 5; j++){
+
+            if(j == 1){
+                cout<<"* ";
+            }
+            else if((i == 1 || i == 4) && j != 4){
+                cout<<"* ";
+            }
+            else if((i == 2 || i == 3) && j == 4){
+                cout<<"* ";
+            }
+            else{
+                cout<<"  ";
+            }
+        }
+        cout<<endl;
+    }
+
+
+    return 0;
+}
