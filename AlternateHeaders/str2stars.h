6d2c6e9f48824580512954865ffe43075576c3a2
Neco <68390369+GrayMergen@users.noreply.github.com>
2020-08-21 21:51:55 +0300

    Update str2stars.h

--- AlternateHeaders/str2stars.h
+++ AlternateHeaders/str2stars.h
@@ -17,10 +17,52 @@ void str2stars(string word){    //This func prints input string as star letters.
 
     string allCapWord = to_upper(word);  //Input word turns into all capital characters
 
-    int i = 0;
+    int i = 0, yaxis = 8, counter = 0;
     while(allCapWord[i]){   //While letters are exist
 
-        if(isalpha(allCapWord[i])){ //This control does if allCapWord[i] is alphabetic. If it is, can print.
+        if(i >= 14){  //Check if letter bigger than output screen and if it is, write in new line
+
+            if(counter == 0){
+               totalWeight = 0;}
+
+                gotoxy(totalWeight, yaxis);
+
+                    if(isalpha(allCapWord[i])){ //This control does if allCapWord[i] is alphabetic. If it is, can print.
+                        for(int row = 0; row < 6; row++){
+                            for(int col = 0; col < 8; col++){
+
+                            if(alphabet[(int)allCapWord[i] - 65][row][col] == 1)    //Detecting which letter will print
+                            cout<<"*";
+
+                    else
+                        cout<<" ";
+                }
+                gotoxy(totalWeight, yaxis + row + 1);   //Creating new line
+            }
+            gotoxy(totalWeight + 8, yaxis);   //Avoid cursor's position end on letter's bottom
+
+            totalWeight += 8;   //After the letter printed total weight increased 8
+        }
+
+        else if(allCapWord[i] == ' '){   //If counting letter is space, must be printed
+
+            for(int row = 0; row < 6; row++){   //These two loop generate 6 rows 3 cols space
+                for(int col = 0; col < 4; col++){
+
+                    cout<<" ";
+                }
+
+                gotoxy(totalWeight, yaxis + row + 1);   //Creating new line
+            }
+            gotoxy(totalWeight + 4, yaxis);   //Avoid cursor's position end on letter's bottom
+
+            totalWeight += 4;   //After the letter printed total weight increased 4
+        }
+
+counter ++;
+}
+
+        else if((isalpha(allCapWord[i]) && (i < 14))){ //This control does if allCapWord[i] is alphabetic. If it is, can print.
 
             for(int row = 0; row < 6; row++){
                 for(int col = 0; col < 8; col++){
@@ -52,8 +94,7 @@ void str2stars(string word){    //This func prints input string as star letters.
 
             totalWeight += 4;   //After the letter printed total weight increased 4
         }
-
-
+        
         i++;    //Loop variable
     }
 
@@ -64,6 +105,9 @@ void str2stars(string word){    //This func prints input string as star letters.
 
                 if(alphabet[(int)allCapWord[i] - 65][row][col] == 1)
                     cout<<"*";
+
+                else if(alphabet[(int)allCapWord[i] - 32] == char(32))
+                    cout<<" ";
 
                 else
                     cout<<" ";
